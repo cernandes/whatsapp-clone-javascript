e2da02079efbaf1cc59e4e52af46c36bc20c1d26
Claudio <cernandes.silva@gmail.com>
2020-07-03 18:12:57 -0300

    metodo para formatar horas, minutos e segundos

--- src/util/format.js
+++ src/util/format.js
@@ -1,6 +1,7 @@
 /**classe estática para gerar o data-set dos elementos e transforma-los para o padrão camel case */
 class Format {
 
+    /* formata todos os IDs separados por traço em camelcase */
     static getCamelCase(text) {
 
         let div = document.createElement('div');
@@ -8,6 +9,24 @@ class Format {
         div.innerHTML = `<div data-${text}="id"></div>`;
 
         return Object.keys(div.firstChild.dataset)[0];
+    }
+
+    /* formata as horas, minutos e segundos */
+    static toTime(duration) {
+
+        let seconds = parseInt((duration / 1000) % 60);
+        let minutes = parseInt((duration / (1000 * 60)) % 60);
+        let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+        if (hours > 0) {
+
+            return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+
+        } else {
+
+            return `${minutes}:${seconds.toString().padStart(2, '0')}`
+        }
+
     }
 
 }
