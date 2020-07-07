2f7808f07aaa91b07f633761f1bf364deab9ffa6
Claudio <cernandes.silva@gmail.com>
2020-07-07 11:34:04 -0300

    ativando o microfone com a API media devices

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,5 +1,6 @@
 import { Format } from './../util/format';
 import { CameraController } from './CameraController';
+import { MicrophoneController } from './MicrophoneController';
 import { DocumentPreviewController } from './DocumentPreviewController';
 
 export class WhatsAppController {
@@ -298,8 +299,6 @@ export class WhatsAppController {
 
                 }).catch(error => {
 
-                    reject(error)
-
                     this.el.panelDocumentPreview.css({
                         'height': 'calc(100% - 120px)'
                     });
@@ -364,16 +363,26 @@ export class WhatsAppController {
             this.el.recordMicrophone.show();
             this.el.btnSendMicrophone.hide();
             this.startRecordMicrophoneTime();
+
+            this._microphoneController = new MicrophoneController()
+
+            this._microphoneController.on('play',()=>{
+
+
+            });
+            
         });
 
         this.el.btnCancelMicrophone.on('click', e => {
 
+            this._microphoneController.stop();
             this.closeRecordMicrophone();
 
         });
 
         this.el.btnFinishMicrophone.on('click', e => {
 
+            this._microphoneController.stop();
             this.closeRecordMicrophone();
 
         });
