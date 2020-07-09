6588976187dabf818e1222452efa2c027f954734
Claudio <cernandes.silva@gmail.com>
2020-07-09 10:15:30 -0300

    capturando e gravando audio do microfone

--- src/controller/MicrophoneController.js
+++ src/controller/MicrophoneController.js
@@ -6,19 +6,19 @@ export class MicrophoneController extends ClassEvent {
 
         super();
 
+        this._mimetype = 'audio/webm';
+
+        this._available = false;
+
         navigator.mediaDevices.getUserMedia({
             audio: true
         }).then(stream => {
 
+            this._available = true;
+
             this._stream = stream;
 
-            let audio = new Audio();
-
-            audio.src = URL.createObjectURL(stream);
-
-            audio.play();
-
-            this.trigger('play', audio);
+            this.trigger('ready', this._stream);
 
         }).catch(error => {
 
@@ -27,11 +27,66 @@ export class MicrophoneController extends ClassEvent {
 
     }
 
+    isAvailable() {
+
+        return this._available;
+    }
+
     stop() {
 
         this._stream.getTracks().forEach(track => {
             track.stop();
         });
+    }
+
+    startRecorder() {
+
+        if (this.isAvailable()) {
+
+            this._mediaRecorder = new MediaRecorder(this._stream, {
+                _mimiType: this._mimetype
+            });
+
+            this._recordedChunks = [];
+
+            this._mediaRecorder.addEventListener('dataavailable', e => {
+
+                if (e.data.size > 0) {
+                    this._recordedChunks.push(e.data);
+                }
+            });
+
+            this._mediaRecorder.addEventListener('stop', e => {
+
+                let blob = new Blob(this._recordedChunks, {
+
+                    type: this._mimetype
+                })
+
+                let filename = `rec${Date.now()}.webm`;
+
+                let file = new File([blob], filename, {
+                    type: this._mimiType,
+                    lastModified: Date.now()
+                });
+
+                console.log('file', file);
+
+            });
+
+            this._mediaRecorder.start();
+        }
+
+    }
+
+    stopRecorder() {
+
+        if (this.isAvailable()) {
+
+            this._mediaRecorder.stop();
+            this.stop();
+        }
+
     }
 
 }
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -366,23 +366,25 @@ export class WhatsAppController {
 
             this._microphoneController = new MicrophoneController();
 
-            this._microphoneController.on('play', audio => {
+            this._microphoneController.on('ready', audio => {
 
-                console.log('recebio evento play', audio)
+                console.log('ready event');
+
+                this._microphoneController.startRecorder();
             });
 
         });
 
         this.el.btnCancelMicrophone.on('click', e => {
 
-            this._microphoneController.stop();
+            this._microphoneController.stopRecorder();
             this.closeRecordMicrophone();
 
         });
 
         this.el.btnFinishMicrophone.on('click', e => {
 
-            this._microphoneController.stop();
+            this._microphoneController.stopRecorder();
             this.closeRecordMicrophone();
 
         });
