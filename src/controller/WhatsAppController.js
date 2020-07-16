c41e5a241ecb915f4164372fe778155282705846
Claudio <cernandes.silva@gmail.com>
2020-07-16 10:24:14 -0300

    scroll automático para as mensagens do chat

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -175,10 +175,15 @@ export class WhatsAppController {
             display: 'flex'
         });
 
+        this.el.panelMessagesContainer.innerHTML = '';
+
         Message.getRef(this._contactActive.chatId).orderBy('timeStamp')
             .onSnapshot(docs => {
 
-                this.el.panelMessagesContainer.innerHTML = '';
+
+                let scrollTop = this.el.panelMessagesContainer.scrollTop;
+                let scrollTopMax = (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight);
+                let autoScroll = (scrollTop >= scrollTopMax);
 
                 docs.forEach(doc => {
 
@@ -186,7 +191,8 @@ export class WhatsAppController {
 
                     data.id = doc.id;
 
-                    if (!this.el.panelMessagesContainer.querySelector('#' + data.id)) {
+                    if (!this.el.panelMessagesContainer.querySelector('#_' + data.id)) {
+
 
                         let message = new Message();
 
@@ -200,6 +206,15 @@ export class WhatsAppController {
                     }
 
                 });
+
+                if (autoScroll) {
+
+                    this.el.panelMessagesContainer.scrollTop = (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight);
+                } else {
+
+                    this.el.panelMessagesContainer.scrollTop = scrollTop;
+
+                }
             });
     }
 
